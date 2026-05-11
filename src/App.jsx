90d6b23f1c790ef7e2eced7a6f82e5d2cc45f342
JeanManyama <jean.manyama@gmail.com>
2026-05-11 02:09:54 +0200

    Modif image

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,13 @@ export default function App() {
         <div className="buttons">
           <a href="https://github.com/JeanManyama" target="_blank">GitHub</a>
           <a href="https://deploy-front-vercel-cd.vercel.app" target="_blank">Projet live</a>
+          <a href="https://github.com/JeanManyama/kalhyge-prod-frontend" target="_blank">
+  Frontend GitHub
+</a>
+
+<a href="https://github.com/JeanManyama/kalhyge-prod-backend" target="_blank">
+  Backend GitHub
+</a>
         </div>
       </header>
 
@@ -31,12 +38,26 @@ export default function App() {
           <li>Security (rate limiting, validation, npm audit)</li>
           <li>Déploiement Vercel + Render</li>
         </ul>
+        <img src="/production-dashboard.png" alt="production-dashboard" className="dashboard-img"/>
       </section>
 
       {/* COMPÉTENCES */}
       <section className="card">
         <h3>🛠️ Compétences</h3>
         <p>React • Node.js • Express • PostgreSQL • Git • CI/CD • API REST • Sécurité</p>
+      </section>
+
+            {/* FONCTIONNALITES*/}
+            <section className="card">
+            <h3>⚙️ Fonctionnalités</h3>
+            <ul>
+  <li>Authentification JWT</li>
+  <li>Suivi de production en temps réel</li>
+  <li>Communication temps réel avec Socket.IO</li>
+  <li>Pipeline CI/CD automatisée</li>
+  <li>Rate limiting et sécurité API</li>
+  <li>Tests unitaires avec Jest</li>
+</ul>
       </section>
 
       {/* CONTACT */}
